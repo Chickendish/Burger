b011ce0c543f0995f6a963216b37748513f4e1b8
Bill Donovan <williamcdonovan@gmail.com>
2017-08-24 11:31:45 -0600

    added routes folder

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  port: 3306,
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+// Dependencies
+var Sequelize = require("sequelize");
+// Creates mySQL connection using Sequelize
+var sequelize = new Sequelize("burgers_db", "root", "password", {
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  console.log("connected as id " + connection.threadId);
 });
-// Export connection for our ORM to use.
-module.exports = connection;
+// Exports the connection for other files to use
+module.exports = sequelize;
